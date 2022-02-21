ba41dc2a67c1eeaf44dc52eb5eeaa49df3082b73
dansenpir <senegalha@protonmail.com>
2022-02-21 19:06:56 -0300

    test: ensure EmailValidatorAdapter calls validator with correct email

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -10,16 +10,23 @@ jest.mock('validator', () => {
 })
 
 describe('EmailValidator Adapter', () => {
-  it('Shoud retunr false if validator returns false', () => {
+  it('Shoud return false if validator returns false', () => {
     const sut = new EmailValidatorAdapter()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_email@mail.com')
     expect(isValid).toBe(false)
   })
 
-  it('Shoud retunr true if validator returns true', () => {
+  it('Shoud return true if validator returns true', () => {
     const sut = new EmailValidatorAdapter()
     const isValid = sut.isValid('valid_email@mail.com')
     expect(isValid).toBe(true)
+  })
+
+  it('Shoud call validator with correct email', () => {
+    const sut = new EmailValidatorAdapter()
+    const isEmailSpy = jest.spyOn(validator, 'isEmail')
+    sut.isValid('any_email@mail.com')
+    expect(isEmailSpy).toHaveBeenCalledWith('any_email@mail.com')
   })
 })
